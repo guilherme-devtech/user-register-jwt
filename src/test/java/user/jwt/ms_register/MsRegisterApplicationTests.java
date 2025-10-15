195956b73e2bb2847975ac7f03dd09fe2bb614ba
guilherm311 <guilherme102031@gmail.com>
2025-10-15 18:46:01 -0300

    Starting project

---
+++ src/test/java/user/jwt/ms_register/MsRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package user.jwt.ms_register;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
